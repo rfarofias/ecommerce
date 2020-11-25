bea72ad7970df88a787294d3b2b6390acae02c1f
Ricardo Farófias <rfarofias@gmail.com>
2020-11-25 18:26:14 +0000

    Ajuste endereço

--- site.php
+++ site.php
@@ -134,6 +134,7 @@
 		}
 
 		if(!$address->getdesaddress()) 		$address->setdesaddress('');
+		if(!$address->getdesnumber()) 		$address->setdesnumber('');
 		if(!$address->getdescomplement())	$address->setdescomplement('');
 		if(!$address->getdesdistrict()) 	$address->setdesdistrict('');
 		if(!$address->getdescity()) 		$address->setdescity('');
